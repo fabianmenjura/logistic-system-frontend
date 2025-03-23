cac1ca215fdd2ca32da1d6ba054cf8b49bb5c6f3
fabianmenjura <53685320+fabianmenjura@users.noreply.github.com>
2025-03-23 01:01:12 -0500

    ajustes iniciales del dashboard

---
+++ src/components/Login.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import api from "../utils/Api";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(username, password);
+      navigate("/home"); // Redirige al usuario a la página de inicio
+    } catch (error) {
+      alert(error.message || "Error al iniciar sesión");
+    }
+  };
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.card}>
+        <h2 style={styles.title}>Iniciar Sesión</h2>
+        <form onSubmit={handleSubmit} style={styles.form}>
+          <input
+            type="text"
+            placeholder="Usuario"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            style={styles.input}
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={styles.input}
+          />
+          <button type="submit" style={styles.button}>Iniciar Sesión</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+// Estilos mejorados
+const styles = {
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    backgroundColor: "#E3E3E3",
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: "30px",
+    borderRadius: "10px",
+    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+    textAlign: "center",
+    width: "350px",
+  },
+  title: {
+    fontSize: "24px",
+    marginBottom: "20px",
+    color: "#333",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  input: {
+    marginBottom: "15px",
+    padding: "12px",
+    fontSize: "16px",
+    borderRadius: "5px",
+    border: "1px solid #ccc",
+    outline: "none",
+    transition: "border 0.3s",
+  },
+  button: {
+    padding: "12px",
+    fontSize: "16px",
+    backgroundColor: "#003c82",
+    color: "#fff",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    transition: "background 0.3s",
+  },
+  buttonHover: {
+    backgroundColor: "#003c82",
+  },
+};
+
+export default Login;
